b3962770f1f6517f1519146f51b597c5c38aff08
itelet <szabo.adam.0725@gmail.com>
2021-04-12 19:59:25 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import React, {Component} from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+
+import { connect } from 'react-redux'
 
 import NavbarComp from './components/Navbar'
 import Main from './components/Main';
 import Signin from './components/Signin'
 import Signup from './components/Signup'
-import Words from './components/Words/index'
+import Dashboard from './components/Dashboard'
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComp />
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/words" component={Words} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { auth, authError } = this.props;
+    return (
+      <div className="App">
+        <NavbarComp />
+        <Switch>
+          {auth.uid ? <Route exact path="/" component={Dashboard} /> : <Route exact path="/" component={Main} />}
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authErr
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './index.css'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div className="main">
+                
+            </div>
+        )
+    }
+}
+
